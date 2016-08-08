fda71eca14c5f8ae813c25628a87fc3a3ab317a0
levinboimtomer <tomerlevinboim@Tomers-MacBook-Pro.local>
2016-08-07 18:51:32 -0700

    susbt

---
+++ susbt.pl
@@ -0,0 +1,30 @@
+use strict;
+
+my $line;
+my $input = shift; # a line with the format "arg1=value1;arg2=value2
+my @assignments = split(';', $input);
+my %D;
+
+# parse the input assignments
+foreach $a (@assignments) {
+    my ($key, $value) = split('=', $a);
+    $key = clean($key);
+    $value = clean($value);
+    $D{$key} = $value;
+}
+
+while ($line = <>) {
+    foreach my $k (keys %D) {
+      my $v = $D{$k};
+      $line =~ s!{$k}!$v!g;
+    }
+    print $line;
+}
+
+sub clean {
+    my $a = shift;
+    $a =~ s/^\s*//;
+    $a =~ s/\s*$//;
+    return $a;
+}
+
